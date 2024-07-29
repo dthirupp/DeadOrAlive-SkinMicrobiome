64aefb5825ea4386f6cf30b2af2ff1248d8c4379
dthirupp <82230529+dthirupp@users.noreply.github.com>
2024-07-29 10:23:28 -0700

    Update and rename avg_chars.sh to avg_readlengths.sh

--- Code/shell/avg_readlengths.sh
+++ Code/shell/avg_readlengths.sh
@@ -2,4 +2,4 @@ while read -r sample; do
 	average=$(awk 'NR >= 2 && (NR - 2) % 4 == 0 { char_count[length]++; total_chars += length; } END { avg = total_chars / (((NR - 2) / 4) + 1); print "Average: " avg }' "${sample}_merged.fq.gz")
 	echo "${sample}\t${average}"   >> ./avg_read_lengths.csv
 	echo "${sample} done"
-done < /media/deepan/Deepan/LiveDead/igm-storage2.ucsd.edu/220428_A00953_0539_AHMNLKDSX3/sample_names_final.txt
+done < /sample_names_final.txt
