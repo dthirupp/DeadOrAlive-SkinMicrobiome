1f99f2a1ec11ee60a09b2f2bc3c6d1cdf9838de5
dthirupp <82230529+dthirupp@users.noreply.github.com>
2025-04-29 17:57:54 -0700

    Delete Code/shell directory

--- Code/shell/avg_readlengths.sh
+++
@@ -1,5 +0,0 @@
-while read -r sample; do
-	average=$(awk 'NR >= 2 && (NR - 2) % 4 == 0 { char_count[length]++; total_chars += length; } END { avg = total_chars / (((NR - 2) / 4) + 1); print "Average: " avg }' "${sample}_merged.fq.gz")
-	echo "${sample}\t${average}"   >> ./avg_read_lengths.csv
-	echo "${sample} done"
-done < /sample_names_final.txt
